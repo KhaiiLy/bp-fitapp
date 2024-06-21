40f348bd70a28987b5fa0e0a9a545a9901c1cfcd
Khai Ly <kaja.lydinh@seznam.cz>
2024-06-21 12:13:40 +0200

    modified variable notes

--- lib/data/workout/workout.dart
+++ lib/data/workout/workout.dart
@@ -5,18 +5,21 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Workout {
   final String? wid;
   final String name;
+  final String notes;
   final dynamic createdAt;
   // final List<Exercise>? exercises;
 
   Workout({
     this.wid,
     required this.name,
+    required this.notes,
     required this.createdAt,
     /*this.exercises*/
   });
 
   Map<String, dynamic> toMap() => {
         'name': name,
+        'notes': notes,
         'createdAt': createdAt,
         // 'exercises': exercises.map((exercise) => exercise.toMap()).toList(),
       };
@@ -24,6 +27,7 @@ class Workout {
   factory Workout.fromMap(String id, Map<String, dynamic> data) => Workout(
         wid: id,
         name: data['name'] ?? '',
+        notes: data['notes'] ?? '',
         createdAt: data['createdAt'] ?? Timestamp.now(),
 
         // exercises: List<Exercise>.from(
